51732cad07198afcb337c52ac1ac6a33d6d7ac02
Christopher Fowers <Christopher Fowers>
2016-05-05 14:14:55 -0400

    updated plugin structure

--- better-polling.js
+++ better-polling.js
@@ -1,5 +1,11 @@
-var betterPolling = {
-    createPoll: function(postData) {
+(function(module){
+   "use strict";
+
+    var Plugin = {};
+
+    Plugin.createPoll = function(postData) {
         alert(postData);
-    }
-};
+    };
+
+    module.exports = Plugin;
+}(module));
